bc7b6016533fd64d592fb494d1b3c168078426cd
fatjonomeri <109109747+fatjonomeri@users.noreply.github.com>
2023-07-13 11:46:13 +0000

    Fetched candidatures

--- react-badges/src/AppEngineer.tsx
+++ react-badges/src/AppEngineer.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import AppEntrypoint, { EngineerIcon } from "./containers/AppEntrypoint";
+import Requirements from "./views/engineer/Requirements";
 
 const AppEngineer: React.FC = () => (
   <AppEntrypoint
     icon={<EngineerIcon />}
     title="Engineer"
-    defaultRoute="dashboard"
+    defaultRoute="engineer"
     routes={[
       {
-        path: "dashboard",
-        element: <div>Engineer</div>
+        path: "requirements",
+        element: <Requirements/>
       }
     ]}
   />
