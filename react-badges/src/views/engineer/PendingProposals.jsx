8bea649612f934c7589ede9a1ae9b988e714f471
omeriii <136507296+omeriii@users.noreply.github.com>
2023-07-24 06:32:14 +0000

    Added buttons in pending proposals

--- react-badges/src/views/engineer/PendingProposals.jsx
+++ react-badges/src/views/engineer/PendingProposals.jsx
@@ -11,7 +11,9 @@ import {
   Box,
   Accordion,
   AccordionSummary,
-  AccordionDetails
+  AccordionDetails,
+  Stack,
+  Divider
 } from "@mui/material";
 import BasicPage from "../../layouts/BasicPage/BasicPage";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
@@ -30,9 +32,34 @@ const GET_PENDING_PROPOSALS = gql`
   }
 `;
 
+const ACCEPTED_DECLINED_PROPOSALS = gql`
+  query MyQuery($engineerId: Int!, $is_approved: Boolean!) {
+    manager_badge_candidature_proposal_response(
+      where: {
+        engineer_to_manager_badge_candidature_proposal: {
+          created_by: { _eq: $engineerId }
+        }
+        is_approved: { _eq: $is_approved }
+      }
+    ) {
+      engineer_to_manager_badge_candidature_proposal {
+        badges_version {
+          title
+          description
+        }
+        user {
+          name
+        }
+        proposal_description
+      }
+    }
+  }
+`;
+
 const PendingProposals = () => {
   const { user_id } = useContext(AuthContext);
   const [proposals, setProposals] = useState([]);
+  const [selectedProposalType, setSelectedProposalType] = useState("pending");
 
   const [getProposals, { loading, error, data }] = useMutation(
     GET_PENDING_PROPOSALS
@@ -51,37 +78,141 @@ const PendingProposals = () => {
     fetchData();
   }, []);
 
+  const {
+    loading: approvedLoading,
+    error: approvedError,
+    data: approvedData
+  } = useQuery(ACCEPTED_DECLINED_PROPOSALS, {
+    variables: {
+      engineerId: user_id,
+      is_approved: true
+    }
+  });
+
+  const {
+    loading: rejectedLoading,
+    error: rejectedError,
+    data: rejectedData
+  } = useQuery(ACCEPTED_DECLINED_PROPOSALS, {
+    variables: {
+      engineerId: user_id,
+      is_approved: false
+    }
+  });
+
+  console.log(
+    "rejectedData",
+    rejectedData?.manager_badge_candidature_proposal_response[0].engineer_to_manager_badge_candidature_proposal
+    .badges_version.id
+  );
+
   if (loading) return "Loading...";
   if (error) return `Error: ${error.message}`;
 
   return (
     <BasicPage fullpage title="Pending Proposals" subtitle="Engineer">
       <br />
-      {data?.get_pending_proposals_for_engineer &&
-        data?.get_pending_proposals_for_engineer.length === 0 && (
-          <Alert severity="info">No pending badges!</Alert>
-        )}
-      <Box mt={2}>
-        {data?.get_pending_proposals_for_engineer?.map((badge) => (
-          <Accordion key={badge.id}>
-            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-              <Typography variant="h3">{badge.badges_version.title}</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography variant="body1">
-                {" "}
-                Proposal description: {badge.proposal_description}
-              </Typography>
-            </AccordionDetails>
-          </Accordion>
-        ))}
-      </Box>
+      <Stack
+        direction="row"
+        divider={<Divider orientation="vertical" flexItem />}
+        spacing={2}
+      >
+        <Button onClick={() => setSelectedProposalType("pending")}>
+          Pending proposals
+        </Button>
+        <Button onClick={() => setSelectedProposalType("approved")}>
+          Accepted proposals
+        </Button>
+        <Button onClick={() => setSelectedProposalType("rejected")}>
+          Declined proposals
+        </Button>
+      </Stack>
+
+      {selectedProposalType === "pending" &&
+      data?.get_pending_proposals_for_engineer?.length === 0 ? (
+        <Alert severity="info">No pending badges!</Alert>
+      ) : selectedProposalType === "pending" ? (
+        <Box mt={2}>
+          {data?.get_pending_proposals_for_engineer?.map((badge) => (
+            <Accordion key={badge.id}>
+              <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography variant="h3">
+                  {badge.badges_version.title}
+                </Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography variant="body1">
+                  {" "}
+                  Proposal description: {badge.proposal_description}
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+          ))}
+        </Box>
+      ) : selectedProposalType === "approved" ? (
+        approvedData?.engineer_to_manager_badge_candidature_proposal.map(
+          (proposal, index) => (
+            <Accordion
+              key={
+                proposal.engineer_to_manager_badge_candidature_proposal
+                  .badges_version.id
+              }
+            >
+              <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography variant="h3">
+                  {
+                    proposal.engineer_to_manager_badge_candidature_proposal
+                      .badges_version.title
+                  }
+                </Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography variant="body1">
+                  {" "}
+                  Proposal description:{" "}
+                  {
+                    proposal.engineer_to_manager_badge_candidature_proposal
+                      .badges_version.proposal_description
+                  }
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+          )
+        )
+      ) : selectedProposalType === "rejected" ? (
+        rejectedData?.engineer_to_manager_badge_candidature_proposal.map(
+          (proposal, index) => (
+            <Accordion
+              key={
+                proposal.engineer_to_manager_badge_candidature_proposal
+                  .badges_version.id
+              }
+            >
+              <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography variant="h3">
+                  {
+                    proposal.engineer_to_manager_badge_candidature_proposal
+                      .badges_version.title
+                  }
+                </Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography variant="body1">
+                  {" "}
+                  Proposal description:{" "}
+                  {
+                    proposal.engineer_to_manager_badge_candidature_proposal
+                      .badges_version.proposal_description
+                  }
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+          )
+        )
+      ) : null}
       <hr />
     </BasicPage>
   );
 };
 
 export default PendingProposals;
-
-
-//commentttt
